186d3b5443e8aaada0834f46253d18069447b884
Sarah Rossy <sarahrossysings@gmail.com>
2021-02-05 00:33:42 +0000

    addProperty postgresql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -148,7 +148,7 @@ const getAllProperties = function(options, limit = 10) {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length}`
 
-  console.log(queryString, queryParams)
+  //console.log(queryString, queryParams)
   return pool.query(queryString, queryParams)
   .then(res => res.rows)
   .catch(error => console.log(error.stack))
@@ -162,9 +162,14 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  // make sure they're in the right order
+  const queryString = `INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, provence, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)  RETURNING *;`
+  
+  const queryParams = [property.owner_id, property.title, property.description, propThproperty.thumbnail_photo_urlumbnail, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+
+  return pool.query(queryString, queryParams)
+    .then(res => res.rows[0])
+    .catch(error => console.log(error.stack))
 }
 exports.addProperty = addProperty;
